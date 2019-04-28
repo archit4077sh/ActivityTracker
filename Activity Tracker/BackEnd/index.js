d65c037dd2e2ec1c8380ede8b01759e41496c04f
Archit <architgupta4077sh@gmail.com>
2019-04-28 07:13:10 +0530

    Comments added

--- Activity Tracker/BackEnd/index.js
+++ Activity Tracker/BackEnd/index.js
@@ -1,6 +1,6 @@
-var express = require('express');
+var express = require('express');//requiring it to creating the server
 var bodyParser = require('body-parser');
-var cors = require('cors');
+var cors = require('cors');//requiring it for recieving post request from server other than hosted at  localhost:3000 (this server)
 
 var connection = require('./connection.js');
 var router = require('./controller/userController.js')
@@ -10,4 +10,4 @@ var app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/' , router);
-app.listen(3000 , function(){console.log('Server Started')});
+app.listen(3000 , function(){console.log('Server Started')});//Started a server
